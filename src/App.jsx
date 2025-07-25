3821918ca3ef465c0785728b34beb410d11e1872
ozodbek2007376 <mirzarahmonovozodbek770gmail.com>
2025-07-25 10:52:07 +0500

    “Hobank”

---
+++ src/App.jsx
@@ -0,0 +1,239 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <div className=" container navbar">
+          <div className="nav">
+            <img src="./img/nav img.png" alt="" />
+            <h1 className="h1">HooBank</h1>
+          </div>
+          <ul className="ul-sty">
+            <li>
+              <a className="a-sty" href="">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="a-sty" href="">
+                About Us
+              </a>
+            </li>
+            <li>
+              <a className="a-sty" href="">
+                Features
+              </a>
+            </li>
+            <li>
+              <a className="a-sty" href="">
+                Solution
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="meta">
+        <div className="box">
+        <div>
+        <h3 className="">20% DISCOUNT FOR 1 MONTH ACCOUNT</h3>
+          <h2 className="h2-sty">
+            The Next <br />
+            <span className="sty"> Generation</span>
+           <br /> Payment Method.
+          </h2>
+          <p className="card-2">
+            Our team of experts uses a methodology to identify the credit cards
+            most likely to fit your needs. We examine annual percentage rates,
+            annual fees.
+          </p>
+        </div>
+      <div>
+      <img className="img" src="./img/Group 481775.png" alt="" />
+      </div>
+        </div>
+        
+      </div>
+      <div className="card">
+      <h1 className="div-sty">3800+ <p className="p-sty">User Active</p> 230+ <p className="p-sty">TRUSTED BY COMPANY</p>  $230M+  <p className="p-sty">TRANSACTION</p> </h1>
+    </div>
+
+
+
+    <div  className="meta-sty">
+      <div>
+        <h2 className="card-1">You do the business, we’ll handle the money.</h2>
+          <p className="card-2">With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.</p>
+          <button className="btn">Get Started</button>
+      </div>
+<div className="d-flex">
+  
+<div className="card-3">
+        <div>
+          <img src="./img/yulduz 1.png" alt="" />
+        </div>
+        <div>
+          <h4 className="h4-sty">Rewards</h4>
+          <p className="card-2" >The best credit cards offer some tantalizing combinations of promotions and prizes</p>
+        </div>
+      </div>
+
+      <div className="card-3">
+        <div>
+          <img src="./img/yu.png" alt="" />
+        </div>
+        <div> 
+          <h4 className="h4-sty">100% Secured</h4>
+          <p className="card-2" >We take proactive steps make sure your information and transactions are secure.</p>
+        </div>
+      </div>
+
+      <div className="card-3">
+        <div>
+          <img src="./img/yulduz.png" alt="" />
+        </div>
+        <div>
+          <h4 className="h4-sty">Balance Transfer</h4>
+          <p className="card-2" >A balance transfer credit card can save you a lot of money in interest charges.</p>
+        </div>
+      </div>
+</div>
+
+    </div>
+
+
+    <div className="me-sty">
+      <div className="section-sty">
+        <img src="./img/1.png" alt="" />
+        <img className="img2" src="./img/2.png" alt="" />
+      </div>
+      <div className="card-7">
+        <h1 className="card-7-h1">Easily control your billing & invoicing.</h1>
+        <p className="card-7-p">Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.</p>
+        <div className="App-img">
+          <img src="./img/App store.png" alt="" />
+          <img src="./img/Play.png" alt="" />
+        </div>
+      </div>
+    </div>
+   
+
+    <div className="card-10">
+    <div>
+        <h2 className="card-1">Find a better card deal in few easy steps.</h2>
+          <p className="card-2">Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.</p>
+          <button className="btn">Get Started</button>
+      </div>
+      <div className="">
+      <img className="bmw-img" src="./img/wEYFI;WO'RGP.png" alt="" />
+      </div>
+    </div>
+
+
+    <div className="main-sty">
+      <h1 className="main-h1">What people are saying about us</h1>
+      <p className="main-p">Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+    </div>
+
+    <div className="df-sty">
+      <div className="main-div">
+        <p className="main-div-p">Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.</p>
+        <div className="main">
+          <img src="./img/odam.png" alt="" />
+        <div className="bg">
+        <h3>Herman Jensen</h3>
+        <p>Founder & Leader</p>
+        </div>
+        </div>
+      </div>
+     
+
+      <div className="main-div">
+        <p className="main-div-p">Money makes your life easier. If you're lucky to have it, you're lucky.</p>
+        <div className="main">
+          <img src="./img/odam2.png" alt="" />
+        <div className="bg">
+        <h3>Steve Mark</h3>
+        <p>Founder & Leader</p>
+        </div>
+        </div>
+      </div>
+
+
+      <div className="main-div">
+        <p className="main-div-p">It is usually people in the money business, finance, and international trade that are really rich.</p>
+        <div className="main">
+          <img src="./img/odam3.png" alt="" />
+        <div className="bg">
+        <h3>Kenn Gallagher</h3>
+        <p>Founder & Leader</p>
+        </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+    <div className="img-meta">
+      <img className="img-meta-img" src="./img/img1.png" alt="" />
+      <img className="img-meta-img" src="./img/img2.png" alt="" />
+      <img className="img-meta-img" src="./img/img3.png" alt="" />
+      <img className="img-meta-img" src="./img/img4.png" alt="" />
+    </div>
+
+
+
+    <div className="benom">
+      <div>
+        <h2 className="benom-1">Let’s try our service now!</h2>
+        <p className="benom-2">Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+      </div>
+      <div><button className="btn">Get Started</button></div>
+    </div>
+
+
+
+    <div className="footer">
+      <div className="footer-1">
+        <img src="./img/footer.svg" alt="" />
+        <p>A new way to make the payments easy, reliable and secure.</p>
+      </div>
+
+      <div className="footer-1">
+        <h3 className="footer-h3">Usefull Links</h3>
+        
+        <p>Content</p>
+        <p>How it Works</p>
+        <p>Create</p>
+        <p>Explore</p>
+        <p>Terms & Services</p>
+      </div>
+      
+      <div className="footer-1">
+        <h3 className="footer-h3">Community</h3>
+        <p>Help Center</p>
+        <p>Partners</p>
+        <p>Suggestions</p>
+        <p>Blog</p>
+        <p>Newsletters</p>
+      </div>
+
+      <div className="footer-1">
+        <h3 className="footer-h3">Partner</h3>
+        <p>Our Partner</p>
+        <p>Become a Partner</p>
+      </div>
+
+    </div>
+
+
+
+
+
+    </>
+  );
+}
+
+export default App;
